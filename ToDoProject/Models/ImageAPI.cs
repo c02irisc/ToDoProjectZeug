9f9f6914ac9bcc6e63d369deaa2db3217e6380e3
Fauxpas <irina.scheucher@edu.campus02.at>
2020-03-11 20:01:30 +0100

    Saving all before unit test

---
+++ ToDoProject/Models/ImageAPI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoProject.Models
+{
+    class ImageAPI
+    {
+
+        public class RootObject
+        {
+            public string id { get; set; }
+            public string author { get; set; }
+            public int width { get; set; }
+            public int height { get; set; }
+            public string url { get; set; }
+            public string download_url { get; set; }
+        }
+
+    }
+}
--- ToDoProject/ViewModels/RestApiViewModel.cs
+++ ToDoProject/ViewModels/RestApiViewModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using static ToDoProject.Models.ImageAPI;
+
 
 namespace ToDoProject.ViewModels
 {
@@ -11,9 +13,7 @@ namespace ToDoProject.ViewModels
     {
 
         public String GeladenesBild { get; set; }
-
-
-
+        
         public RestApiViewModel()
         {
             GeladenesBild = GetPicture();
@@ -21,27 +21,29 @@ namespace ToDoProject.ViewModels
 
         public String GetPicture()
         {
-            
-            String resultBild = "hallo";
-            
+            String resultBild = "https://picsum.photos/id/";
+
             HttpClient client = new HttpClient();
-            string path = "https://picsum.photos/id/0/info";
+            string path = "https://picsum.photos/v2/list?page=2&limit=100";
 
             HttpResponseMessage response = client.GetAsync(path).Result;
 
             if (response.IsSuccessStatusCode)
             {
-                Console.WriteLine(response);
                 var stringcontent = response.Content.ReadAsStringAsync().Result;
-                Console.WriteLine(stringcontent);
-                Console.WriteLine("Test");
+                List<RootObject> pictures = Newtonsoft.Json.JsonConvert.DeserializeObject<List<RootObject>>(stringcontent);
+                
+                Random random = new Random();
+                int p = random.Next(0, pictures.Count);
+
+                String url = pictures[p].download_url;
+                url = url.Substring(0, url.LastIndexOf("/"));
+                url = url.Substring(0, url.LastIndexOf("/"));
+
+                resultBild = url + "/300/300?blur=5";
             }
 
-            
             return resultBild;
-           
-
         }
-
     }
 }
